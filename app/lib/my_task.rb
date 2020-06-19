3e8426b08f209b36266594d0d0a3bc8216a509b7
Peter Maneykowski <pmaneyko@gmail.com>
2020-06-19 12:29:22 -0500

    fix logging and add safety new for using too much disk storage

--- app/lib/my_task.rb
+++ app/lib/my_task.rb
@@ -10,12 +10,26 @@ class MyTask
   end
 
   def self.run(job_result_id)
-    Thread.new do
-      job_result = JobResult.find job_result_id
-      job_result.running!
-      result = %x{ps aux --sort -rss | awk '{n+=$4}END{print n}' && sleep 15}.chomp
-      job_result.update_attribute(:result, {memory_usage_percent: result})
-      job_result.not_running!
+    unless self.disk_usage > 80
+      Thread.new do
+        job_result = JobResult.find job_result_id
+        job_result.running!
+        result = %x{ps aux --sort -rss | awk '{n+=$4}END{print n}' && sleep 15}.chomp
+        job_result.update_attribute(:result, {memory_usage_percent: result})
+        job_result.not_running!
+      end
+    else
+      Rails.logger.error "Disk usage above 80%"
     end
+  end
+
+  def self.disk_usage
+    command = %q{
+      df --output=used,avail \
+      | sort \
+      | tail -1 \
+      | perl -ne 'printf("%.1f", $1/$2 * 100) if /([\d]+)[\s]+([\d]+)/'
+    }
+    %x{#{command}}.to_f
   end
 end
